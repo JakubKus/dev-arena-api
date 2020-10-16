3ce30790771fdd3ecbb612fd25af4c8e79486a06
Jakub <kubakus97@o2.pl>
2020-10-16 18:59:01 +0200

    feat: add player type

---
+++ graphql/models/player.js
@@ -0,0 +1,15 @@
+const mongo = require('mongoose');
+const Schema = mongo.Schema;
+
+const schema = new Schema({
+  nickname: String,
+  mail: String,
+  cash: Number,
+  wonFights: Number,
+  comboTime: Number,
+  chosenDevId: String,
+  equippedIds: Array.of(String),
+  boughtIds: Array.of(String)
+});
+
+module.exports = mongo.model('player', schema);
---
+++ graphql/types/playerType.js
@@ -0,0 +1,18 @@
+const { GraphQLFloat, GraphQLID, GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLList } = require('graphql');
+
+const playerType = new GraphQLObjectType({
+  name: 'player',
+  fields: () => ({
+    id: { type: GraphQLID },
+    nickname: { type: GraphQLString },
+    mail: { type: GraphQLString },
+    cash: { type: GraphQLFloat },
+    wonFights: { type: GraphQLInt },
+    comboTime: { type: GraphQLInt },
+    chosenDevId: { type: GraphQLString },
+    equippedIds: { type: new GraphQLList(GraphQLString) },
+    boughtIds: { type: new GraphQLList(GraphQLString) }
+  })
+});
+
+module.exports = playerType;
